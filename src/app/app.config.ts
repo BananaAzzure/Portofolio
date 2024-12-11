25ae905c7f1c0ca9bf823dd493aef96563e6f7e4
kontol <kemalalif57@gmail.com>
2024-12-11 09:30:11 +0700

    Portofolio

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,9 +8,11 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { icons } from './icons-provider';
+import { provideNzIcons } from 'ng-zorro-antd/icon';
 
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(), provideNzIcons(icons)]
 };
